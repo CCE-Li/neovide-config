5080f5c2d2948b3f46cadcb39996a3a0a190d6f2
CCE-Li <2766503189@qq.com>
2026-02-25 17:46:44 +0800

    修改：将tab键重新绑定、修复退出文件时反复确认文件是否修改

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -21,9 +21,26 @@ return {
           { name = "buffer" },    -- 缓冲区单词补全
         }),
         mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.select_next_item(),  -- Tab 选下一个
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),-- Shift+Tab 选上一个
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()  -- 没有补全菜单时执行默认 Tab 行为（缩进）
+            end
+          end, { "i", "s", "c" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()  -- 没有补全菜单时执行默认 Shift+Tab 行为
+            end
+          end, { "i", "s", "c" }),
+          ["<Down>"] = cmp.mapping.select_next_item(),  -- 下箭头选下一个
+          ["<Up>"] = cmp.mapping.select_prev_item(),    -- 上箭头选上一个
+          ["<C-n>"] = cmp.mapping.select_next_item(),    -- Ctrl+n 选下一个
+          ["<C-p>"] = cmp.mapping.select_prev_item(),    -- Ctrl+p 选上一个
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- 回车确认
+          ["<C-Space>"] = cmp.mapping.complete(),       -- Ctrl+Space 手动触发补全
         }),
       })
     end,
@@ -36,7 +53,16 @@ return {
     dependencies = { 'rafamadriz/friendly-snippets' },
     version = '1.*',
     opts = {
-      keymap = { preset = 'super-tab' },
+      keymap = { 
+        preset = 'super-tab',  -- 使用 super-tab 预设，智能 Tab 行为
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-Space>'] = { 'show', 'show_documentation', 'fallback' },
+      },
       appearance = {
         nerd_font_variant = 'mono'
       },
